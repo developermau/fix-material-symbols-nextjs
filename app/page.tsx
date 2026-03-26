616876de2acfe2769581b1d92132e64d1b740ebb
Alejandro <11723245+developermau@users.noreply.github.com>
2026-03-26 02:40:26 +0000

    Add more details

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
 import Image from "next/image";
 import { SectionMaterialSymbols } from "./ui/sections/section-material-symbols";
+import { SectionRemixIcons } from "./ui/sections/section-remix-icons";
 
 export default function Home() {
   return (
     <>
+      <h1 className="text-xl">
+        Next.js Integration with Font Icons (Material Symbols, and Remix Icons)
+      </h1>
       <SectionMaterialSymbols />
+      <SectionRemixIcons />
     </>
   );
 }
