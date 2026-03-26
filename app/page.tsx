f033f0023c92bf47f44b9c8afa019f7f4a49ebcd
Alejandro <11723245+developermau@users.noreply.github.com>
2026-03-26 02:35:56 +0000

    refactor material symbols

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,10 @@
 import Image from "next/image";
+import { SectionMaterialSymbols } from "./ui/sections/section-material-symbols";
 
 export default function Home() {
   return (
-    <main className="p-5 flex flex-col gap-2">
-      <h1 className="text-3xl font-bold">Material Symbols & Icons</h1>
-      <div className="flex gap-2">
-        <span className="font-bold">Home:</span>
-        <span className="material-symbols-outlined">home</span>
-      </div>
-      <div className="flex gap-2">
-        <span className="font-bold">Star:</span>
-        <span className="material-symbols-outlined">star</span>
-      </div>
-      <div className="flex gap-2">
-        <span className="font-bold">Stacks:</span>
-        <span className="material-symbols-outlined text-red-500">stacks</span>
-      </div>
-    </main>
+    <>
+      <SectionMaterialSymbols />
+    </>
   );
 }
